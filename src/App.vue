dc1512b7afd806c869b390095017d0d3f9d75448
chachagof <chacha0932@gmail.com>
2023-07-20 04:19:26 +0800

    feat:add font awesome

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <h1>hello world</h1>
   <button type="button" class="btn btn-info">Info</button>
+  <button class="btn btn-primary btn-lg" type="button">Call Me <font-awesome-icon :icon="['fas', 'phone']" /> </button>
+  <font-awesome-icon :icon="['fas', 'phone']" />
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPhone)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
