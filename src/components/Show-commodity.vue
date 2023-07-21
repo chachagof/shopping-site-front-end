df1697d2d6de70c4b1e06cb4ac36586509c24768
chachagof <chacha0932@gmail.com>
2023-07-21 21:55:34 +0800

    feat:add components show commodity

---
+++ src/components/Show-commodity.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="commodity-container col-10 d-flex flex-wrap justify-content-center">
+    <!-- each commodity -->
+    <div class="card col-2 m-3">
+      <img src="https://fakeimg.pl/200x200/" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">commodity name</h5>
+        <div class="button-container d-flex justify-content-between">
+          <router-link to="/info" class="btn btn-outline-success"><font-awesome-icon icon="circle-info" /></router-link>
+        <router-link to="/cart" class="btn btn-outline-warning"><font-awesome-icon icon="cart-plus" /></router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
