f9db6478a0d5c80c0374c46b2fb4469764b6959e
chachagof <chacha0932@gmail.com>
2023-07-22 20:33:35 +0800

    feat:add home function 1.import commodities data 2.add commodities modal (showModal) 3.add commodity to cart (addToCart)

--- src/components/Show-commodity.vue
+++ src/components/Show-commodity.vue
@@ -1,37 +1,63 @@
+<script setup>
+import { ref, defineProps } from 'vue'
+
+defineProps(['commoditiesData'])
+
+const isModalVisible = ref(false)
+const modalData = ref(null)
+
+const showModal = (commodity) => {
+  if (!isModalVisible.value) {
+    isModalVisible.value = true
+    modalData.value = [commodity]
+  } else {
+    isModalVisible.value = false
+  }
+}
+
+const addToCart = (commodityId) => {
+  console.log('add', commodityId)
+  // add to cart
+  // cart id
+}
+</script>
+
 <template>
   <div class="commodity-container col-10 d-flex flex-wrap justify-content-center">
     <!-- each commodity -->
-    <div class="card col-2 m-3">
-      <img src="https://fakeimg.pl/200x200/" class="card-img-top">
+    <div class="card col-2 m-3" v-for="commodity in commoditiesData" :key="commodity.id">
+      <img :src="commodity.avatar" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">commodity name</h5>
+        <h5 class="card-title">{{commodity.name}}</h5>
         <div class="button-container d-flex justify-content-between">
-          <router-link to="/info" class="btn btn-outline-success"><font-awesome-icon icon="circle-info" /></router-link>
-        <router-link to="/cart" class="btn btn-outline-warning"><font-awesome-icon icon="cart-plus" /></router-link>
+          <button @click="showModal(commodity)" class="btn btn-outline-success"><font-awesome-icon icon="circle-info" /></button>
+          <button @click="addToCart(commodity.id)" class="btn btn-outline-warning"><font-awesome-icon icon="cart-plus" /></button>
         </div>
       </div>
     </div>
   </div>
-  <div class="modal-container">
-    <div class="card text-center">
+  <div class="modal-container" v-if="isModalVisible">
+    <div class="card text-center col-4" >
       <div class="card-header">
-        Featured
+        <p class="text-center m-1 fs-4">
+          {{ modalData[0].name }}
+        </p>
       </div>
       <div class="card-body">
         <img src="https://fakeimg.pl/200x200/">
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text mt-5">商品描述:{{ modalData[0].description }}</p>
+        <p class="card-text mb-5">商品價格:{{ modalData[0].price }}</p>
       </div>
       <div class="card-footer text-muted">
         <div class="button-container d-flex justify-content-between">
-          <router-link to="/back" class="btn btn-outline-danger">Back   <font-awesome-icon icon="backward" /></router-link>
-          <router-link to="/cart" class="btn btn-outline-warning">Add  To Cart <font-awesome-icon icon="cart-plus" /></router-link>
+          <button @click="showModal" class="btn btn-outline-danger">Back   <font-awesome-icon icon="backward" /></button>
+          <button @click="addToCart(modalData[0].id)" class="btn btn-outline-warning">Add  To Cart <font-awesome-icon icon="cart-plus" /></button>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style>
-/* 根據需求自行設定Modal的樣式 */
 .modal-container {
   position: fixed;
   top: 0;
--- src/views/Home-page.vue
+++ src/views/Home-page.vue
@@ -1,7 +1,46 @@
 <script setup>
+import { ref } from 'vue'
 import Commodity from '../components/Show-commodity.vue'
+const dummyData = ref([
+  {
+    id: 1,
+    name: 'bbb',
+    description: 'Good',
+    price: 696,
+    inventory: 10,
+    avatar: 'https://fakeimg.pl/200x200/',
+    sellerId: 1,
+    categoryId: 1,
+    createdAt: '2023-07-19T07:59:32.000Z',
+    updatedAt: '2023-07-19T07:59:32.000Z'
+  },
+  {
+    id: 2,
+    name: 'aaa',
+    description: 'Good',
+    price: 100,
+    inventory: 10,
+    avatar: 'https://fakeimg.pl/200x200/',
+    sellerId: 1,
+    categoryId: 1,
+    createdAt: '2023-07-19T07:59:43.000Z',
+    updatedAt: '2023-07-19T07:59:43.000Z'
+  },
+  {
+    id: 3,
+    name: 'ccc',
+    description: '我愛阿莉',
+    price: 9999,
+    inventory: 10,
+    avatar: 'https://fakeimg.pl/200x200/',
+    sellerId: 1,
+    categoryId: 1,
+    createdAt: '2023-07-19T07:59:52.000Z',
+    updatedAt: '2023-07-19T07:59:52.000Z'
+  }
+])
 </script>
 
 <template>
-  <Commodity/>
+  <Commodity :commoditiesData="dummyData"/>
 </template>
