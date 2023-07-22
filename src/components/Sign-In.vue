fa3f52ba0a37d6d0d5963c3627bc77df05fdc611
chachagof <chacha0932@gmail.com>
2023-07-23 01:12:12 +0800

    feat:add buyer signin function

--- src/components/Sign-In.vue
+++ src/components/Sign-In.vue
@@ -1,24 +1,43 @@
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue'
+
+defineProps(['role'])
+const emit = defineEmits(['form-submitted'])
+
+const account = ref('')
+const password = ref('')
+
+const handleSubmit = () => {
+  const userData = {
+    account: account.value,
+    password: password.value
+  }
+  emit('form-submitted', userData)
+}
+
+</script>
+
 <template>
   <div class="signin col-5">
     <div class="mb-4 text-center">
-      <h1 class="h3 font-weight-normal mb-3">Sign In</h1>
+      <h1 v-if="role[0].role === 'buyer'" class="h3 font-weight-normal mb-3">Buyer Sign In</h1>
     </div>
-    <form class="form-signin" action="/signin" method="POST">
+    <form @submit.prevent="handleSubmit" class="form-signin">
       <div class="form-row mb-3">
         <label class="form-label" for="account">Account</label>
-        <input class="form-control" id="account" type="text" name="account" placeholder="Account" required
+        <input v-model="account" class="form-control" id="account" type="text" name="account" placeholder="Account" required
           autofocus>
       </div>
       <div class="form-row mb-3">
         <label class="form-label" for="password">Password</label>
-        <input class="form-control" id="password" type="password" name="password"
+        <input v-model="password" class="form-control" id="password" type="password" name="password"
           placeholder="Password" required>
       </div>
       <button class="btn btn-primary btn-block" type="submit">Submit</button>
     </form>
     <div class="mb-4 text-center">
-      <p>
-        <router-link to="/signup">
+      <p v-if="role[0].role === 'buyer'">
+        <router-link to="/buyer/signup">
           Sign Up
         </router-link>
       </p>
