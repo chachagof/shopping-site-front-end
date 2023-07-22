71ed58b73c56815393e5da6b420ec8ba901a11c0
chachagof <chacha0932@gmail.com>
2023-07-23 01:23:59 +0800

    feat:add buyer signup function

--- src/components/Sign-up.vue
+++ src/components/Sign-up.vue
@@ -1,33 +1,56 @@
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue'
+
+defineProps(['role'])
+const emit = defineEmits(['form-submitted'])
+
+const name = ref('')
+const account = ref('')
+const password = ref('')
+const passwordCheck = ref('')
+
+const handleSubmit = () => {
+  const userData = {
+    name: name.value,
+    account: account.value,
+    password: password.value,
+    passwordCheck: passwordCheck.value
+  }
+  emit('form-submitted', userData)
+}
+</script>
+
 <template>
   <div class="signup col-5 ">
     <div class="mb-4 text-center">
-      <h1 class="h3 font-weight-normal mb-3">Sign Up</h1>
+      <h1 v-if="role[0].role === 'buyer'" class="h3 font-weight-normal mb-3">Buyer Sign Up</h1>
     </div>
-    <form class="form-signup" action="/signup" method="POST">
+    <form @submit.prevent="handleSubmit" class="form-signup">
       <div class="form-row mb-3">
         <label class="form-label" for="name">Name</label>
-        <input type="text" id="name" name="name" class="form-control" placeholder="Name" required autofocus>
+        <input v-model="name" type="text" id="name" name="name" class="form-control" placeholder="Name" required autofocus>
       </div>
       <div class="form-row mb-3">
         <label class="form-label" for="account">Account</label>
-        <input type="text" id="account" name="account" class="form-control" placeholder="Account" required>
+        <input v-model="account" type="text" id="account" name="account" class="form-control" placeholder="Account" required>
       </div>
       <div class="form-row mb-3">
         <label class="form-label" for="password">Password</label>
-        <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+        <input v-model="password" type="password" id="password" name="password" class="form-control" placeholder="Password" required>
       </div>
       <div class="form-row mb-3">
         <label class="form-label" for="passwordCheck">Password Check</label>
-        <input type="password" id="passwordCheck" name="passwordCheck" class="form-control" placeholder="Password Check"
+        <input v-model="passwordCheck" type="password" id="passwordCheck" name="passwordCheck" class="form-control" placeholder="Password Check"
           required>
       </div>
       <button class="btn btn-primary btn-block" type="submit">Submit</button>
     </form>
     <div class="mb-4 text-center">
-      <p>
-        <router-link to="/signin">
+      <p v-if="role[0].role === 'buyer'">
+        <router-link to="/buyer/signup">
           Sign In
-        </router-link></p>
+        </router-link>
+      </p>
     </div>
   </div>
 </template>
--- src/views/Buyer-signup.vue
+++ src/views/Buyer-signup.vue
@@ -1,7 +1,14 @@
 <script setup>
+import { ref } from 'vue'
 import Signup from '../components/Sign-up.vue'
+
+const roleData = ref([{ role: 'buyer' }])
+
+const buyerData = (data) => {
+  console.log(data)
+}
 </script>
 
 <template>
-  <Signup/>
+  <Signup :role="roleData" @form-submitted="buyerData"/>
 </template>
