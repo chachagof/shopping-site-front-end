97697b31f0a4ecbf812373680007c67757005766
chachagof <chacha0932@gmail.com>
2023-07-23 01:14:23 +0800

    feat:add seller signin function

--- src/components/Sign-In.vue
+++ src/components/Sign-In.vue
@@ -21,6 +21,7 @@ const handleSubmit = () => {
   <div class="signin col-5">
     <div class="mb-4 text-center">
       <h1 v-if="role[0].role === 'buyer'" class="h3 font-weight-normal mb-3">Buyer Sign In</h1>
+      <h1 v-if="role[0].role === 'seller'" class="h3 font-weight-normal mb-3">Seller Sign In</h1>
     </div>
     <form @submit.prevent="handleSubmit" class="form-signin">
       <div class="form-row mb-3">
@@ -38,6 +39,11 @@ const handleSubmit = () => {
     <div class="mb-4 text-center">
       <p v-if="role[0].role === 'buyer'">
         <router-link to="/buyer/signup">
+          Sign Up
+        </router-link>
+      </p>
+      <p v-if="role[0].role === 'seller'">
+        <router-link to="/seller/signup">
           Sign Up
         </router-link>
       </p>
--- src/views/Seller-signin.vue
+++ src/views/Seller-signin.vue
@@ -1,7 +1,14 @@
 <script setup>
+import { ref } from 'vue'
 import SignIn from '../components/Sign-In.vue'
+
+const roleData = ref([{ role: 'seller' }])
+
+const sellerData = (data) => {
+  console.log(data)
+}
 </script>
 
 <template>
-  <SignIn/>
+  <SignIn :role="roleData" @form-submitted="sellerData"/>
 </template>
