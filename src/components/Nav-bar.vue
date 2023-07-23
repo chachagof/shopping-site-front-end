526b892a98dd4df0f5faa3a6584e0fc6c5d5ea7c
chachagof <chacha0932@gmail.com>
2023-07-23 16:22:32 +0800

    feat:add navbar function

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const commodity = ref('')
+const shop = ref('')
+const category = ref('')
+const price = ref('')
+const lowPrice = ref('')
+const highPrice = ref('')
+const searchOption = ref('')
+
+const handleSubmit = () => {
+  const searchData = {
+    commodity: commodity.value,
+    shop: shop.value,
+    category: category.value,
+    price: price.value,
+    lowPrice: lowPrice.value,
+    highPrice: highPrice.value,
+    searchOption: searchOption.value
+  }
+  console.log(searchData)
+}
+</script>
+
 <template>
   <nav class="navbar navbar-light bg-light p-3">
     <!-- logo -->
@@ -7,49 +32,49 @@
     <!-- searchbar -->
     <div class="searchbar-container col-6">
       <div class="searchbar">
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <form @submit.prevent="handleSubmit" class="d-flex">
+          <input v-model="searchOption" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
       <div class="search-category d-flex align-items-center justify-content-between mt-2">
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="commodity" checked>
+          <input v-model="commodity" class="form-check-input" type="radio" name="flexRadioDefault" id="commodity">
           <label class="form-check-label" for="commodity">
             commodity
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="shop" >
+          <input v-model="shop" class="form-check-input" type="radio" name="flexRadioDefault" id="shop" >
           <label class="form-check-label" for="shop">
             shop
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="category">
+          <input v-model="category" class="form-check-input" type="radio" name="flexRadioDefault" id="category">
           <label class="form-check-label" for="category">
             category
           </label>
         </div>
         <div class="form-check d-flex align-items-center ">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="above-price">
+          <input v-model="price" class="form-check-input" type="radio" name="flexRadioDefault" id="above-price">
           <label class="form-check-label me-2 ms-2" for="above-price">
             price
           </label>
           <div class="price-range d-flex align-items-center  col-6">
             <label class="form-check-label" for="minPrice"></label>
-            <input class="form-control me-2" type="number" id="minPrice" name="minPrice" step="1" min="0">
+            <input v-model="lowPrice" class="form-control me-2" type="number" id="minPrice" name="minPrice" step="1" min="0">
             <label class="form-check-label  me-2" for="maxPrice">to</label>
-            <input class="form-control" type="number" id="maxPrice" name="maxPrice" step="1" min="0">
+            <input v-model="highPrice" class="form-control" type="number" id="maxPrice" name="maxPrice" step="1" min="0">
           </div>
         </div>
       </div>
     </div>
     <!-- button -->
     <div class="buttons-container">
-      <button type="button" class="btn btn-outline-warning me-3">
-        Cart<font-awesome-icon icon="cart-shopping" />
-      </button>
+      <router-link to="/cart" class="btn btn-outline-warning me-3">
+          Cart<font-awesome-icon icon="cart-shopping" />
+      </router-link>
       <button type="button" class="btn btn-outline-success">
         Login<font-awesome-icon icon="right-to-bracket" />
       </button>
